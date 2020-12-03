4f140f0f9b3d27776756d4f5c700a9a434754152
Andrey Kim <erel3.69@gmail.com>
2020-12-03 23:28:15 +0600

    bot02: round1 started

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 g++ bot/bot.cpp -o strategies/bot || exit -1
-rewind-client/main.py -p ../strategies/bot 12 10 -p ../strategies/bot 0 0 -f default
+rewind-client/main.py -p ../strategies/bot 0 0 -p ../strategies/bot02 12 10 -f default
