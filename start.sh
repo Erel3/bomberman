1ce2024f8dada66674bfaca088aae3f16d5be467
Andrey Kim <erel3.69@gmail.com>
2020-12-09 21:00:24 +0600

    added harakiri

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-g++ bot/bot_08.cpp -o strategies/bot_08 -std=c++17 || exit -1
-rewind-client/main.py -p ../strategies/bot_08 0 0 -p ../strategies/bot02 12 10 -f default
-# rewind-client/main.py -p ../strategies/bot_08 0 0 -p ../strategies/bot02 12 10 -f default -a 1000
-# rewind-client/main.py -p ../strategies/bot_08 0 0 -p ../strategies/bot02 12 10 -f default --replay ../games/08_12_2020_1607444373810.gamelog
+g++ bot/bot_09.cpp -o strategies/bot_09 -std=c++17 || exit -1
+rewind-client/main.py -p ../strategies/bot_09 0 0 -p ../strategies/bot_02 12 10 -f default
+# rewind-client/main.py -p ../strategies/bot_09 0 0 -p ../strategies/bot_02 12 10 -f default -a 1000
+# rewind-client/main.py -p ../strategies/bot_09 0 0 -p ../strategies/bot_02 12 10 -f default --replay ../games/09_12_2020_1607505018494.gamelog
