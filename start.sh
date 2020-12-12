f3d5985d4a67937a43ab5bbf4337c1846efcb1d4
Andrey Kim <erel3.69@gmail.com>
2020-12-12 20:52:43 +0600

    test commit

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 g++ bot/bot_12.mika.cpp -o strategies/bot_12.mika -std=c++17 || exit -1
 rewind-client/main.py -p ../strategies/bot_12.mika 0 0 -p ../strategies/bot_09 12 10 -f file
 # rewind-client/main.py -p ../strategies/bot_12.mika 0 0 -p ../strategies/bot_09 12 10 -f file -a 100
-# rewind-client/main.py -p ../strategies/bot_12.mika 0 0 -p ../strategies/bot_09 12 10 --replay ../games/12_12_2020_1607776232465.gamelog
+# rewind-client/main.py -p ../strategies/bot_12.mika 0 0 -p ../strategies/bot_09 12 10 --replay ../games/12_12_2020_1607776232465.gamelo
