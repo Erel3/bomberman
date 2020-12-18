671f5cb5d65025a90de8df344518c2588ffda4cb
Andrey Kim <erel3.69@gmail.com>
2020-12-18 18:32:50 +0600

    bot 18 released

---
+++ start1.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+g++ bot/bot_17.mika.cpp -o strategies/bot_17.mika -std=c++17 || exit -1
+# g++ bot/bot_12.mika.cpp -o strategies/bot_12.mika -std=c++17 || exit -1
+
+# rewind-client/main.py -p ../strategies/bot_17.mika 0 0 -p ../strategies/bot_12.mika 12 10 -f file
+rewind-client/main.py -p ../strategies/bot_17.mika 0 0 -p ../strategies/bot_12.mika 12 10 -f file -a 50 --proc 2
+# rewind-client/main.py -p ../strategies/bot_17.mika 0 0 --replay ../games/18_12_2020_1608274856873.gamelog
+
+
+# ? waited.. for what?? ../games/18_12_2020_1608250976775.gamelog
+# ? https://cup.alem.school/main/game/replay/36e5c963-d46f-467a-8b20-9790accea79a
