63e814bced55a68041418d9a7ca3cd3c6b8fbe49
Andrey Kim <erel3.69@gmail.com>
2020-12-19 14:22:23 +0600

    enemy_dist

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
-# g++ bot/bot_18.mika.cpp -o strategies/bot_18.mika -std=c++17 || exit -1
-# g++ bot/bot_12.mika.cpp -o strategies/bot_12.mika -std=c++17 || exit -1
+g++ bot/bot_19.opt.cpp -o strategies/bot_19.opt -std=c++17 || exit -1
+# g++ bot/bot_17.mika.cpp -o strategies/bot_17.mika -std=c++17 || exit -1
 
-g++ bot/bot_18.mika.cpp -o strategies/bot_18.mika -std=c++17 -DDEBUG_TIME || exit -1
+# g++ bot/bot_19.opt.cpp -o strategies/bot_19.opt -std=c++17 -DDEBUG_TIME || exit -1
 
-rewind-client/main.py -p ../strategies/bot_18.mika 0 0 -p ../strategies/bot_12.mika 12 10 -f file
-# rewind-client/main.py -p ../strategies/bot_18.mika 0 0 -p ../strategies/bot_17.mika 12 10 -f file -a 50 --proc 2
-# rewind-client/main.py -p ../strategies/bot_18.mika 0 0 --replay ../games/18_12_2020_1608274856873.gamelog
+# rewind-client/main.py -p ../strategies/bot_19.opt 0 0 -p ../strategies/bot_17.mika 12 10 -f file
+# rewind-client/main.py -p ../strategies/bot_19.opt 0 0 -p ../strategies/bot_17.mika 12 10 -f file -a 100 --proc 2
+rewind-client/main.py -p ../strategies/bot_19.opt 0 0 --replay ../games/19_12_2020_1608362543179.gamelog
 
 
 # ? waited.. for what?? ../games/18_12_2020_1608250976775.gamelog
